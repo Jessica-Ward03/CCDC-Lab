d85693a4ba9d18737c7b8a33203fe9f1103a057f
Jessica-Ward03 <jessicaward.jw03@gmail.com>
2025-10-07 19:47:44 -0400

    Updated gitignore

--- InjectTimer.py
+++ InjectTimer.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import itertools
 import os.path
 import time
-
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
