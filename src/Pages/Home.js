f57f6de67a2bf58f1b82cd0e6e7241af1e67d473
Jessica-Ward03 <jessicaward.jw03@gmail.com>
2025-11-30 22:00:29 -0500

    Added Timers

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,8 @@
 import "./Home.css";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useTimer } from "../Time/TimerContext";
 
 const Tab = styled.button`
   font-size: 20px;
@@ -20,9 +22,85 @@ const Tab = styled.button`
 const ButtonGroup = styled.div`
   display: flex;
 `;
-const types = ["First Time Here", "How To Use", "Ready To Enter The Competition"];
-function TabGroup() {
+
+const types = ["First Time Here", "How To Use", "Ready To Enter The Competition"]; 
+
+function TabGroup({ setTutorialMode, startTimer }) {
   const [active, setActive] = useState(types[0]);
+  function renderContent(tab) {
+    switch (tab) {
+      case "First Time Here":
+        return (
+          <div className="homePage">
+            <h3>First Time Here</h3>
+            <h4>What is CCDC?</h4>
+            <p>
+              Its a cyber security competition where students work to manage and protect an existing network.
+            </p>
+            <a href="https://www.nationalccdc.org/about.html" target="_blank" rel="noopener noreferrer">
+              Find out more about CCDC here! 
+            </a>
+            <h4>What is this lab?</h4>
+            <p>
+              This is a lab that models the CCDC lab. This is so that students can learn how to manage and protect a network. It also allows CCDC teams to practice for the CCDC.
+            </p>
+            <p>
+              This lab was created by Jonah Facer, Andrew Nale, John Aaron, and Jessica Ward
+            </p>
+            <a href="https://github.com/Jessica-Ward03/CCDC-Lab" target="_blank" rel="noopener noreferrer">
+              Visit our repo here!
+            </a>
+            <p>
+              If you are reading this section we recommend starting with our tutorial mode
+            </p>
+          </div>
+        );
+
+      case "How To Use":
+        return (
+          <div className="homePage">
+            <h4>How To Use?</h4>
+            <p>ToDo with images for examples?</p>
+
+            <div className="homePageSubButton">
+              <Link to="./Injects">
+                <button onClick={() => {setTutorialMode(1); startTimer(600);}}>Tutorial Mode</button>
+              </Link>
+              <Link to="./Injects">
+                <button onClick={() => {setTutorialMode(2); startTimer(600);}}>Competition Mode</button>
+              </Link>
+            </div>
+          </div>
+        );
+
+      case "Ready To Enter The Competition":
+        return (
+          <div className="homePage">
+            <h3>Ready To Enter The Competition</h3>
+            <div className="noCentering">
+              <p>We recommend double checking{" "} 
+                  <a href="https://github.com/Jessica-Ward03/CCDC-Lab" target="_blank" rel="noopener noreferrer">
+                     here
+                   </a>
+               {" "}for updates
+              </p>
+            </div>
+            <div className="homePageSubButton">
+              <Link to="./Injects">
+                <button onClick={() => {setTutorialMode(1); startTimer(600);}}>Tutorial Mode</button>
+              </Link>
+              <Link to="./Injects">
+                <button onClick={() => {setTutorialMode(2); startTimer(600);}}>Competition Mode</button>
+              </Link>
+            </div>
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  }
+
   return (
     <div className="homePage">
       <ButtonGroup>
@@ -36,87 +114,25 @@ function TabGroup() {
           </Tab>
         ))}
       </ButtonGroup>
-        <div>
+
+      <div>
         {renderContent(active)}
       </div>
     </div>
   );
 }
 
-  function renderContent(active) {
-  switch (active) {
-    case "First Time Here":
-      return (
-        <div className="homePage">
-          <h3>First Time Here</h3>
-          <h4>What is CCDC?</h4>
-
-          <p>
-          Its a cyber security competition where students work to manage and protect an existing network.
-          </p>
-
-          <a href="https://www.nationalccdc.org/about.html" target="_blank" rel="noopener noreferrer">
-            Find out more about CCDC here! 
-          </a>
-
-          <h4>What is this lab?</h4>
-          <p>
-          This is a lab that models the CCDC lab. This is so that students can learn how to manage and protect a network. It also allows CCDC teams to pratice for the CCDC.
-          </p>
-          <p>
-          This lab was created by Jonah Facer, Andrew Nale, John Aaron, and Jessica Ward
-          </p>
-          <a href="https://github.com/Jessica-Ward03/CCDC-Lab" target="_blank" rel="noopener noreferrer">
-            Visit our repo here!
-          </a>
-
-          <p>
-          If you are reading this section we recommend starting with our tutorial mode
-          </p>
-        </div>
-      );
-
-    case "How To Use":
-      return (
-        <div className="homePage">
-          <h4>How To Use?</h4>
-          <p>
-          ToDo  with images for examples?
-          </p>
-          {/*TODO* make buttons once both modes are ready*/}
-          <p>button for lab.............................button for tutorial</p>
-        </div>
-      );
-    case "Ready To Enter The Competition":
-      return (
-        <div className="homePage">
-          <h3>Ready To Enter The Competition</h3>
-
-          <div className="noCentering">
-            <p>We recommend double checking{" "} 
-                <a href="https://github.com/Jessica-Ward03/CCDC-Lab" target="_blank" rel="noopener noreferrer">
-                   here
-                 </a>
-             {" "}for updates
-            </p>
-          </div>
-          {/*TODO* make buttons once both modes are ready*/}
-          <p>button for lab.............................button for tutorial</p>
-        </div>
-      );
-
-    default:
-      return null;
-  }
-
-}
 
 export default function Home() {
+  /* 0 for not defined 1 for tutorial mode 2 for non tutorial mode */
+      const [TutorialMode, setTutorialMode] = useState(0);
+      const {startTimer} = useTimer();
   return(
+  /*Top of the Home page*/ 
   <div className="homePage">
     <h1>IUS CCDC Lab</h1>
     <h2>Welcome to the IUS CCDC Lab </h2>
-    <TabGroup />;
+     <TabGroup setTutorialMode={setTutorialMode} startTimer={startTimer}/>
   </div>
   )
  
