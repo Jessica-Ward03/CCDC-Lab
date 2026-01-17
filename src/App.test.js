1e0aa4d51ebdbb2084b4e1cf55343c63d86c6af6
Jessica-Ward03 <jessicaward.jw03@gmail.com>
2026-01-16 19:44:13 -0500

    added test cases for banner

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+//Checks that the header works on all pages
+test("Header On All Pages", () => {
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText(/Home/i)).toBeInTheDocument();
+  expect(screen.getByText(/Injects/i)).toBeInTheDocument();
+  expect(screen.getByText(/Scoreboard/i)).toBeInTheDocument();
+}
+);
